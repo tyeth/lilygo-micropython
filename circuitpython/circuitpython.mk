818399c6191aaa0b8916e1a52d66af58e21a9ca7
tyeth <tyethgundry@googlemail.com>
2023-09-08 21:06:45 +0100

    Update CircuitPython action and version

--- circuitpython/circuitpython.mk
+++ circuitpython/circuitpython.mk
@@ -6,8 +6,8 @@ define circuitpython/info
 	PKG_NAME:=circuitpython
 	PKG_SOURCE_URL:=https://github.com/adafruit/circuitpython.git
 	PKG_SOURCE_PROTO:=git
-	PKG_SOURCE_VERSION:=8.0.0-beta.3
-	PKG_SOURCE_MD5:=9c8e6a8698f0fbce48ca3f864c9cf38b3905c2da
+	PKG_SOURCE_VERSION:=8.2.5
+	PKG_SOURCE_MD5:=4dd45b6dc6e00eea20a8b03c9fb3131cfb2bb7ee
 	# PKG_SOURCE_OPT:=--recurse-submodules --depth=1
 endef
 
